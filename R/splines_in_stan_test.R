9ece72c185a04e68f6d31905df4b7d64c815f109
jdaeth274 <joshdaeth@yahoo.co.uk>
2018-05-30 16:52:16 +0100

    testing out forming splines in stan itself

---
+++ R/splines_in_stan_test.R
@@ -0,0 +1,240 @@
+###################################################################################################################################################
+## Working with the splines being created in stan itself ##########################################################################################
+###################################################################################################################################################
+
+require(rstan)
+require(splines)
+require(ggplot2)
+rstan_options(auto_write=T)
+options(mc.cores =  parallel::detectCores())
+
+expose_stan_functions("C:/Users/josh/Dropbox/hiv_project/simpleepp/stan_files/chunks/splines_in_stan_simpleepp.stan")
+
+
+
+run_simulated_model<-function(params,times){
+  
+  mu <- params$mu                               # Non HIV mortality / exit from population
+  sigma <- params$sigma           # Progression from stages of infection
+  mu_i <- params$mu_i                                   #c(0.003, 0.008, 0.035, 0.27)     # Mortality by stage, no ART
+  iota<-params$iota
+  
+  
+  dt <- times$dt                                     # time step
+  nsteps <- as.integer(times$years/dt)                   # number of steps
+  xstart <- times$start                                # the start of the epidemic
+  step_vector <- seq(xstart+dt, by=dt, length.out=nsteps)  # steps
+  xout <- c(xstart, step_vector)    
+  
+  
+  
+  rlogistic <- function(t, p) {
+    p[1] - (p[1] - p[2]) / (1 + exp(-p[3] * (t - p[4]))) 
+  }
+  
+  bell_curve_func<-function(a,b,c,x){
+    f_t<-a*exp(-((x-b)^2)/(2*c^2))
+    
+  }
+  
+  kappa_params<-c(log(params$kappa[1]),log(params$kappa[2]),params$kappa[3],params$kappa[4])
+  kappa<-exp(rlogistic(step_vector,kappa_params))
+  
+  #kappa<-bell_curve_func(0.5,0.2,0.2,seq(0,0.998,0.002))
+  
+  mod<-simpleepp_no_art(kappa = kappa,iota,mu,sigma,mu_i,dt)
+  sim_mod<-data.frame(mod)
+  names(sim_mod)<-c("kappa","lambda","prevalence")
+  sim_mod$prev_percent<-sim_mod$prevalence * 100
+  sim_mod$time<-c(xstart,step_vector)
+  
+  return(list(sim_df=sim_mod,kappa_values=kappa))
+  
+  
+}
+
+mu <- 1/35                               # Non HIV mortality / exit from population
+sigma <- 1/c(3.16, 2.13, 3.20)           # Progression from stages of infection
+mu_i <- c(0.003, 0.008, 0.035, 0.27)     # Mortality by stage, no ART
+kappa<-c(0.5,0.1,0.3,1995)
+iota<-0.0001
+
+
+dt <- 0.1                                     # time step
+nsteps <- as.integer(50/dt)                   # number of steps
+xstart <- 1970                                # the start of the epidemic
+step_vector <- seq(xstart+dt, by=dt, length.out=nsteps)  # steps
+xout <- c(xstart, step_vector)                #the vector of steps in total
+
+
+
+params_sim<-list(mu=mu,sigma=sigma,mu_i=mu_i,kappa=kappa,iota=iota)
+times_sim<-list(dt=dt,years=50,start=xstart)
+
+sim_model_output_changed_to_bell_curve<-run_simulated_model(params_sim,times_sim)
+
+sim_plot<-function(sim_df){
+  
+  prev_plot<-ggplot(data = sim_df)+geom_line(aes(x=time,y=prev_percent),colour="midnightblue",size=1.05)+
+    labs(x="Time",y="Prevalence %",title="Simulated model prevalence over time")
+  
+  incidence_plot<-ggplot(data = sim_df)+geom_line(aes(x=time,y=lambda),colour="midnightblue",size=1.05)+
+    labs(x="Time",y="Incidence",title="Simulated model incidence over time")
+  
+  kappa_plot<-ggplot(data = sim_df)+geom_line(aes(x=time,y=kappa),colour="midnightblue",size=1.05)+
+    labs(x="Time",y="kappa",title="Kappa parameter over time in simulated data")
+  
+  melt_df<-sim_df[,-c(4)]
+  melted_df_funky<-melt(melt_df,id="time")
+  
+  three_variable_plot<-ggplot(data = melted_df_funky)+geom_line(aes(x=time,y=value,colour=variable),size=1.05)+
+    labs(x="Time",y="Value",title="Simulated model output")
+  
+  return(list(prevalence=prev_plot,incidence=incidence_plot,kappa=kappa_plot,whole=three_variable_plot))
+  
+  
+}
+
+plotted_sim<-sim_plot(sim_model_output_changed_to_bell_curve$sim_df)
+plot(plotted_sim$whole)
+
+###################################################################################################################################
+## Now we have simulated through our model we can extract some samples form it ####################################################
+###################################################################################################################################
+
+sample_range<-1970:2015
+sample_years<-46
+sample_n<-1000
+
+
+sample_function<-function(year_range,number_of_years_to_sample,people_t0_sample,simulated_df,prevalence_column_id){
+  sample_years_hiv <- number_of_years_to_sample # number of days sampled throughout the epidemic
+  sample_n <- people_t0_sample # number of host individuals sampled per day
+  
+  # Choose which days the samples were taken. 
+  # Ideally this would be daily, but we all know that is difficult.
+  sample_time_hiv = sort(sample(year_range, sample_years_hiv, replace=F))
+  
+  # Extract the "true" fraction of the population that is infected on each of the sampled days:
+  sample_propinf_hiv = simulated_df[simulated_df$time %in% sample_time_hiv, prevalence_column_id]
+  
+  ## this just samples our prevalence, to get a probability that the sample we take is HIV infected then we need to divide
+  ## by 100
+  
+  #sample_propinf_hiv<-sample_propinf_hiv/100
+  
+  # Generate binomially distributed data.
+  # So, on each day we sample a given number of people (sample_n), and measure how many are infected.
+  # We expect binomially distributed error in this estimate, hence the random number generation.
+  sample_y_hiv_prev = rbinom(sample_years_hiv, sample_n, sample_propinf_hiv)
+  sample_prev_hiv_percentage<-(sample_y_hiv_prev/sample_n)*100
+  
+  ## lets have a ggplot of the y (infected) and out sample of Y over time 
+  sample_df_100<-data.frame(cbind(sample_time_hiv,sample_prev_hiv_percentage,sample_y_hiv_prev,sample_time_hiv,sample_propinf_hiv))
+  return(sample_df_100)  
+}
+
+
+
+sample_df_1000_second<-sample_function(sample_range,sample_years,sample_n,
+                                       simulated_df = sim_model_output$sim_df,prevalence_column_id = 3)
+
+
+
+
+plot_sample<-function(sample_df,simulated_df){
+  a<-ggplot(data = simulated_df,aes(x=time,y=prev_percent))+geom_line(colour="midnightblue",size=1.2)+
+    geom_point(data=sample_df, aes(x=sample_time_hiv,y=sample_prev_hiv_percentage),colour="red",size=1)
+  
+  return(plot(a))
+}
+
+plot_sample(simulated_df = sim_model_output$sim_df,sample_df = sample_df_1000_second)
+
+
+ggplot(data = sample_df_1000_second,aes(x=sample_time_hiv,y=sample_prev_hiv_percentage))+geom_point(colour="red",size=1.5)
+
+###################################################################################################################################
+## Now we have our sample from the simulated data we can call the stan script to sample from this data ############################
+###################################################################################################################################
+
+splines_creator<-function(knot_number,penalty_order){
+  
+  nk <- knot_number # number of knots
+  dk <- diff(range(xout))/(nk-3)
+  knots <- xstart + -3:nk*dk
+  spline_matrix<- splineDesign(knots, step_vector, ord=4)
+  penalty_matrix <- diff(diag(nk), diff=penalty_order)
+  
+  return(list(spline_matrix=spline_matrix,penalty_matrix=penalty_matrix))
+  
+}
+
+knot_number= 7
+penalty_order= 2
+
+splines_matrices<-splines_creator(knot_number,penalty_order)
+rows_to_evaluate<-0:45*10+1
+
+
+num_knots <- 7
+spline_degree <- 3
+num_basis <- num_knots + spline_degree - 1
+X <- seq(from=1970, to=2020, by=0.1)
+num_data <- length(X)
+knots <- unname(quantile(X,probs=seq(from=0, to=1, length.out = num_knots)))
+a0 <- 0.2
+a <- rnorm(num_basis, 0, 5)
+B_true <- t(bs(X, df=num_basis, degree=spline_degree, intercept = TRUE))
+Y_true <- as.vector(a0*X + a%*%B_true)
+Y <- Y_true + rnorm(length(X), 0, 0.2)
+
+stan_data_splines<-list(
+  n_obs = sample_years,
+  n_sample = sample_n,
+  y = as.array(sample_df_1000_second$sample_y_hiv_prev),
+  time_steps_euler = length(xout),
+  penalty_order = penalty_order,
+  knot_number = knot_number,
+  estimate_years = 5,
+  time_steps_year = 51,
+  X_design = splines_matrices$spline_matrix,
+  D_penalty = splines_matrices$penalty_matrix,
+  mu = mu,
+  sigma = sigma,
+  mu_i = mu_i,
+  dt = 1,
+  dt_2 = 0.1,
+  rows_to_interpret = as.array(rows_to_evaluate),
+  num_data = num_data,
+  knots = knots,
+  spline_degree = spline_degree,
+  Y = Y,
+  X = X
+)
+
+spline_mod<-stan_model("C:/Users/josh/Dropbox/hiv_project/simpleepp/stan_files/chunks/splines_in_stan_simpleepp.stan")
+test<-sampling(spline_mod,stan_data_splines,iter=500)
+test_extract<-rstan::extract(test)
+test_extract$fitted_output
+prev_median<-(apply(test_extract$fitted_output[,,2],2,median))
+prev_low<-(apply(test_extract$fitted_output[,,2],2,quantile,probs=c(0.025)))
+prev_high<-(apply(test_extract$fitted_output[,,2],2,quantile,probs=c(0.975)))
+spline_produced_estimate_df<-cbind.data.frame(prev_low,prev_median,prev_high)
+names(spline_produced_estimate_df)<-c("low","median","high")
+
+ggplot(data = spline_produced_estimate_df)+geom_line(aes(x=seq(0,100,1),y=median),colour="dodgerblue",size=1.05)+
+  geom_ribbon(aes(x=seq(0,100,1),ymin=low,ymax=high),colour="dodgerblue",fill="dodgerblue",alpha=0.15,size=1.02)+
+  geom_line(aes(x=seq(0,100,1),y=Y_true),colour="firebrick1",size=1.05)+geom_point(aes(x=seq(0,100,1),y=Y),colour="gold",)
+
+
+knots
+spline_degree
+num_knots
+num_data
+Y
+X
+
+
+
+
---
+++ stan_files/chunks/splines_in_stan_simpleepp.stan
@@ -0,0 +1,185 @@
+
+functions{
+
+matrix simpleepp_no_art(vector kappa, real iota, real mu, vector sigma,   // So in this case we have @kappa as our R parameter, @iota as our initial fraction infected
+		 vector mu_i, real dt){                                           // @mu is our population death rate, @sigma is our progression rate through CD4 stages
+                                                                          // @mu_i is our death rate for each of the cd4 stages, the length of this also determines the number of stages
+                                                                          // and finally @dt is our time step, in Euler this is one tenth of the time step 																		  
+  vector[rows(kappa)+1] S;                                                 
+  matrix[rows(mu_i), rows(kappa)+1] I;
+  vector[rows(kappa)+1] rho;                                              // rho is our I/N so when multipled by kappa it becomes our force of infection 
+  vector[rows(kappa)+1] lambda;                                           // This is our force of infection 
+  int DS;
+  DS = rows(mu_i);
+
+  // initial values
+  S[1] = 1000000 * (1 - iota);
+  I[1, 1] = 1000000 * iota;
+  for(m in 2:DS)
+    I[m, 1] = 0;
+    
+  rho[1] = 1 - S[1] / (S[1] + sum(I[,1]));
+  lambda[1] = 0;
+
+  for(t in 1:rows(kappa)) {
+    real artcov;
+    real deaths;
+    real infections;
+    real It;
+    
+
+    It = sum(I[,t]);
+    
+    
+    lambda[t+1] = kappa[t] * rho[t];
+
+    deaths = mu * (S[t] + It) + sum(mu_i .* I[,t]);
+    S[t+1] = S[t] + dt*(-lambda[t+1] * S[t] - mu * S[t] + deaths);                            // 
+
+    I[1, t+1] = I[1, t] + dt*(lambda[t+1] * S[t] - (mu + mu_i[1] + sigma[1]) * I[1, t]);
+    for(m in 2:(DS-1))
+      I[m, t+1] = I[m, t] + dt*(sigma[m-1] * I[m-1, t] - (mu + mu_i[m] + sigma[m]) * I[m, t]);
+    I[DS, t+1] = I[DS, t] + dt*(sigma[DS-1] * I[DS-1, t] - (mu + mu_i[DS]) * I[DS, t]);
+    
+        
+    rho[t+1] = 1.0 - S[t+1] / (S[t+1] + sum(I[ ,t+1]) );
+  }
+  
+  
+  return(append_col(append_row(kappa[1], kappa),
+		    append_col(lambda, rho)));
+}
+ vector build_b_spline(real[] t, real[] ext_knots, int ind, int order);
+  vector build_b_spline(real[] t, real[] ext_knots, int ind, int order) {
+    // INPUTS:
+    //    t:          the points at which the b_spline is calculated
+    //    ext_knots:  the set of extended knots
+    //    ind:        the index of the b_spline
+    //    order:      the order of the b-spline
+    vector[size(t)] b_spline;
+    vector[size(t)] w1 = rep_vector(0, size(t));
+    vector[size(t)] w2 = rep_vector(0, size(t));
+    if (order==1)
+      for (i in 1:size(t)) // B-splines of order 1 are piece-wise constant
+        b_spline[i] = (ext_knots[ind] <= t[i]) && (t[i] < ext_knots[ind+1]);
+    else {
+      if (ext_knots[ind] != ext_knots[ind+order-1])
+        w1 = (to_vector(t) - rep_vector(ext_knots[ind], size(t))) /
+             (ext_knots[ind+order-1] - ext_knots[ind]);
+      if (ext_knots[ind+1] != ext_knots[ind+order])
+        w2 = 1 - (to_vector(t) - rep_vector(ext_knots[ind+1], size(t))) /
+                 (ext_knots[ind+order] - ext_knots[ind+1]);
+      // Calculating the B-spline recursively as linear interpolation of two lower-order splines
+      b_spline = w1 .* build_b_spline(t, ext_knots, ind, order-1) +
+                 w2 .* build_b_spline(t, ext_knots, ind+1, order-1);
+    }
+    return b_spline;
+  }
+}
+
+data {
+ 
+ int<lower = 1> n_obs;                                                  // The number of time points we observe
+ int<lower = 1> n_sample;                                               // The number of people we observe from the population
+ int<lower = 0, upper = n_sample> y[n_obs];                             // This is the number of poeple infected from our random draw 
+ int time_steps_euler;                                                  // This is our number of points over which to evaulate the function 
+ int time_steps_year;                                                   // This is our number of years we evaluate  
+ int penalty_order;                                                     // This is the degree of penalty we place on the 
+ int knot_number;                                                       // Number of knots, our length of beta         
+ int estimate_years;                                                    // This is the number of years we predict data for
+ matrix[time_steps_euler - 1 , knot_number  ] X_design;                 // This is our spline design matirx that we are modelling kappa with. 
+ matrix[knot_number - penalty_order , knot_number ] D_penalty;          // This is our penalty matrix, can be first or second order depending on the R code 
+ real mu;                                                               // This is our population level death rate
+ vector[3] sigma;                                                       // This is our vector of transition rates
+ vector[4] mu_i;                                                        // This is our vector of death rates
+ real dt;                                                               // This is our time step
+ real dt_2;                                                             // this is our second time step
+ int rows_to_interpret[time_steps_year - estimate_years ];              // This is a vector of the rows to use for the prevalence stats in y_hat. Corresponds to whole years
+ vector[knot_number] knots;                                             // the sequence of knots
+ int spline_degree;                                                     // the degree of spline (is equal to order - 1)
+ real X[time_steps_euler];                                              // Our vector of time points over which to evaluate the B spline
+}
+ 
+ transformed data {
+  int num_basis = knot_number + spline_degree - 1;                        // total number of B-splines
+  matrix[num_basis, time_steps_euler] B;                                  // matrix of B-splines
+  vector[spline_degree + knot_number] ext_knots_temp;
+  vector[2*spline_degree + knot_number] ext_knots;                        // set of extended knots
+  ext_knots_temp = append_row(rep_vector(knots[1], spline_degree), knots);
+  ext_knots = append_row(ext_knots_temp, rep_vector(knots[knot_number], spline_degree));
+  for (ind in 1:num_basis)
+    B[ind,:] = to_row_vector(build_b_spline(X, to_array_1d(ext_knots), ind, spline_degree + 1));
+  B[knot_number + spline_degree - 1, time_steps_euler] = 1;
+}
+ 
+parameters{
+ 
+ real<lower = 0, upper=1> iota;                                         // The proportion of the population initially infected 
+ vector<lower = 0>[knot_number] beta;                                   // This is the knot point values
+ real<lower = 0> sigma_pen;                                             // This is the penalty to apply to the spline to make it smooth
+ row_vector[num_basis] a_raw;
+  real a0;                                                              // intercept
+  real<lower=0> sigma_spline;
+  real<lower=0> tau;
+ 
+}
+
+ 
+transformed parameters{
+
+  vector[rows(X_design)] kappa;
+  matrix[size(rows_to_interpret), 3] y_hat;
+  row_vector[num_basis] a;                                             // spline coefficients
+  vector[time_steps_euler] splino;
+  
+  a = a_raw*tau;
+  splino = a0*to_vector(X) + to_vector(a*B);
+
+  kappa = X_design * beta;
+  y_hat = simpleepp_no_art( splino , iota, mu, sigma, mu_i, dt_2)[rows_to_interpret, ];
+
+ 
+ }
+
+ 
+model {
+  
+   
+  a_raw ~ normal(0, 1);                                                  // Priors
+  a0 ~ normal(0, 1);
+  tau ~ normal(0, 1);
+  sigma_spline ~ normal(0, 1);
+
+  
+ 
+ iota ~ normal(0, 0.5);                                                  // This models our initial population size 
+ 
+ target += normal_lpdf( D_penalty * to_vector(a)[2 : num_basis - 1] | 0, sigma_pen);                // So this models our penalized spline with a slightly altered distribution command
+
+
+  y ~ binomial(n_sample, y_hat[ , 3]);                                   // This fits it to the binomially sampled data 
+
+} 
+
+generated quantities{
+
+vector[rows(X_design)] kappa_fit;
+matrix[time_steps_euler , 3] fitted_output;
+
+
+kappa_fit = X_design * beta;
+fitted_output = simpleepp_no_art(kappa_fit, iota, mu, sigma, mu_i, dt_2);       // This models the data after our fitting procedure
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
